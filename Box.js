83bfaf6e79bd5e5a7478b801e4fa5d02aa99c128
Kanak1016 <70909340+Kanak1016@users.noreply.github.com>
2020-10-20 16:44:10 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,34 @@
+class Box {
+
+    constructor(x,y,width,height) {
+    var option = {
+         restitution: 0.5,
+         density : 0.5,
+         friction : 1.0,
+     }       
+     this.body = Bodies.rectangle(x,y,width,height,option);
+     this.width = width;
+     this.height = height;
+     
+     World.add(world, this.body)
+
+     
+    }
+
+    display() {
+    var pos =  this.body.position;
+    var angle = this.body.angle;
+
+    Matter.Body.setAngle(this.body, angle);
+
+    push (); // New drawing has been enabled
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect(0,0,this.width,this.height);   
+    pop () // End of new drawing
+
+    }
+
+
+}
